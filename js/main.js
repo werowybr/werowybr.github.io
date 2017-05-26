185ef06c53f37d15d5d6d2508c4413b1848436fe
jente vh <werowybr@student.arteveldehs.be>
2017-05-26 12:40:43 +0200

    About, contact

---
+++ js/main.js
@@ -0,0 +1,5 @@
+document.querySelector('#header-bars button').addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector('body > nav').style.display = document.querySelector('body > nav').style.display === 'block' ? 'none' : 'block';
+});
+
