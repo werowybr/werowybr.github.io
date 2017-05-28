78dd2787bcfeaf8bada80b0b8c93fcd7facde077
jente vh <werowybr@student.arteveldehs.be>
2017-05-28 16:33:46 +0200

    responsive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
-document.querySelector('#header-bars button').addEventListener('click', function (e) {
-    e.preventDefault();
-    document.querySelector('body > nav').style.display = document.querySelector('body > nav').style.display === 'block' ? 'none' : 'block';
-});
+
+if (document.querySelector('#header-bars button')){
+    document.querySelector('#header-bars button').addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector('body > nav').style.display = document.querySelector('body > nav').style.display === 'block' ? 'none' : 'block';
+    });
+}
+
 
